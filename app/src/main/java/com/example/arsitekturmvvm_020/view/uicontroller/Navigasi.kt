c9c6c4379847232c3fe63377a229893c365b31ea
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-21 16:13:45 +0700

    menambah nilai modifier pada fungsi

--- app/src/main/java/com/example/arsitekturmvvm_020/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/arsitekturmvvm_020/view/uicontroller/Navigasi.kt
@@ -4,13 +4,18 @@ package com.example.arsitekturmvvm_020.view.uicontroller
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.arsitekturmvvm_020.model.DataJK.JenisK
 import com.example.arsitekturmvvm_020.view.FormIsian
 import com.example.arsitekturmvvm_020.view.TampilData
+import com.example.arsitekturmvvm_020.viewmodel.SiswaViewModel
 
 enum class Navigasi {
     Formulirku,
@@ -19,16 +24,20 @@ enum class Navigasi {
 
 @Composable
 fun DataApp(
+    modifier: Modifier,
     navController: NavHostController = rememberNavController()
 ){
     Scaffold { isiRuang->
+
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
 
             modifier = Modifier.padding(paddingValues = isiRuang)){
             composable(route = Navigasi.Formulirku.name) {
+
                 FormIsian(
+
                     OnSubmitBtnClick = {
                         navController.navigate(Navigasi.Detail.name)
                     }
