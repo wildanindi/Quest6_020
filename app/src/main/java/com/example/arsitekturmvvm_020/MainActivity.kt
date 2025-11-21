a53b7dae4f36e92c5e5a07f0e74d01e25c21a7fc
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-21 16:23:55 +0700

    Mengubah setContent pada MainActivity.kt untuk menjalankan aplikasi melalui uicontroller yaitu fungsi DataApp

--- app/src/main/java/com/example/arsitekturmvvm_020/MainActivity.kt
+++ app/src/main/java/com/example/arsitekturmvvm_020/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.arsitekturmvvm_020.ui.theme.ArsitekturMVVM_020Theme
+import com.example.arsitekturmvvm_020.view.uicontroller.DataApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +22,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ArsitekturMVVM_020Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    DataApp(modifier = Modifier)
                 }
             }
         }
