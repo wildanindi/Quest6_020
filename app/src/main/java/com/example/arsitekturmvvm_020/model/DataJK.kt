638bb76445e69aae450e5f119675fa2d0620623e
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-21 15:15:22 +0700

    menambah konstanta(data yang sudah tetap(valueable)) dengan nama JenisK

--- app/src/main/java/com/example/arsitekturmvvm_020/model/DataJK.kt
+++ app/src/main/java/com/example/arsitekturmvvm_020/model/DataJK.kt
@@ -1,4 +1,11 @@
 package com.example.arsitekturmvvm_020.model
 
+import com.example.arsitekturmvvm_020.R
+
+
 object DataJK {
+    val JenisK= listOf(
+        R.string.laki,
+        R.string.wanita
+    )
 }
