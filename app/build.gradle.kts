35c09f4aa8fdb97f79574cf911937308c2189d99
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-21 15:06:47 +0700

    menambah dependencies untuk libs yang di tambahkan tadi, yaitu viewmodel dan runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
     implementation(libs.androidx.navigation.compose)
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
